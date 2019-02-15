bea0a22e3b568c3b18f01311d79a6fe106a3a2e0
bobnearents <bobnearents@yahoo.com>
2019-02-15 12:17:05 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  BASE_URL = 'https://swapi.co/api/'
+  constructor(props){
+    super(props);
+    this.state = {
+      input: '',
+      results: '',
+      handleChange: (value) => {
+        console.log(value);
+        this.setState({
+          input:value
+        })
+      },
+      handleSubmit: (value) => {
+        console.log(value)
+      }
+    }
+  }
+
+  // fetch(`{this.BASE_URL}/people/1`, {
+  //   method:'GET',
+  //   headers: {'Content-Type': 'application/json'}
+  // })
+  // .then(res=>res.json())
+  // .then(person => {
+  
+      //})
+  
+  render() {  
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form onSubmit={(e) => {e.preventDefault();this.state.handleSubmit(this.state.input)}}>
+            <label>
+              Search for STAR WARS: <input type = "text" onChange = {(e) => {this.state.handleChange(e.target.value)}}/>
+            </label>
+            <button type = "submit">Submit</button>
+          </form>
         </header>
       </div>
     );
